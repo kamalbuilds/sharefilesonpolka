87e9f2a48bf0cc917d11e0855fa2355914d92a0e
Kamal Nayan <geniusamansingh@gmail.com>
2024-05-31 14:19:35 +0530

    landing page setup

---
+++ app/providers.tsx
@@ -0,0 +1,24 @@
+"use client";
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/system";
+import { useRouter } from "next/navigation";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
+import { Web3Provider } from "@/components/Web3Provider";
+
+export interface ProvidersProps {
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
+}
+
+export function Providers({ children, themeProps }: ProvidersProps) {
+  const router = useRouter();
+
+  return (
+    <Web3Provider>
+      <NextUIProvider navigate={router.push}>
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </NextUIProvider>
+    </Web3Provider>
+  );
+}
